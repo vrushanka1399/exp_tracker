2cf31e2a3324e1d7af77dd4514aebcba6561f884
vrushanka1399 <vrushanka.1si17cs132@gmail.com>
2026-01-16 10:55:04 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,11 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBo8Pwmbti299m4tq4c--iyz2pdn5uB2S8",
   authDomain: "ecommerce-auth-c5f4b.firebaseapp.com",
+  databaseURL: "https://ecommerce-auth-c5f4b-default-rtdb.firebaseio.com/",
   projectId: "ecommerce-auth-c5f4b",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const database = getDatabase(app);
